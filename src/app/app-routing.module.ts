1eb9f327a85179b7a294e85363aff62717dffece
caique@jamsoft.com.br <caique@jamsoft.com.br>
2021-04-24 11:01:09 -0300

    iniciando projeoto front

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CadastroClienteComponent } from './pages/cadastro-cliente/cadastro-cliente.component';
+import { ListaClientesComponent } from './pages/lista-clientes/lista-clientes.component';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'listaclientes', component: ListaClientesComponent },
+  { path: 'cadastrocliente', component: CadastroClienteComponent },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ListaClientesComponent } from './pages/lista-clientes/lista-clientes.component';
+import { CadastroClienteComponent } from './pages/cadastro-cliente/cadastro-cliente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ListaClientesComponent,
+    CadastroClienteComponent
   ],
   imports: [
     BrowserModule,
